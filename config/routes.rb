660885e4f170dd79679c782a8937a361561c1034
Barry Blanc <barryblanc731@gmail.com>
2021-05-22 18:11:41 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/signup', to: 'users#new'
-  
+  resources :users
 end
